15aa2442fb3bdc5caf0f0234dca64dce729219ee
JakeDuke <jakedukin@gmail.com>
2018-05-28 13:36:35 +0200

    readme added

---
+++ README.md
@@ -0,0 +1,8 @@
+# Descripton
+
+Small app based on Pokemon API. I used Vue for front-end and Axios for HTTP requests.
+The main purpose of this app was to understand HTTP request-responce basics.
+
+## How to use it
+
+Just choose Pokemon's number from 1 to 960 and get it's name, type and image.
--- main.js
+++ main.js
@@ -3,30 +3,19 @@ var vue = new Vue({
     data: {
         name: '',
         picUrl: '',
+        type: '',
         pockid: 1
     },
-
-    // watch: {
-    //     pockid: function (newValue, oldValue) {
-    //         // console.log("OldValue: " + oldValue + " ;NewValue: " + newValue);
-    //         if (newValue != undefined || newValue != "") {
-    //             vue.picUrl = vue.getpock(newValue)
-    //         }
-    //     }
-    // },
-
 
     methods: {
 
         getpock: function (id) {
             axios.get('https://pokeapi.co/api/v2/pokemon/' + id)
                 .then(response => {
-                    // console.log(response.data.forms[0].name);
-                    // console.log(response.data.sprites[front_default]);
+
                     console.log(response.data.name);
-
-
                     vue.name = response.data.name;
+                    vue.type = response.data.types[1]['type']['name'];
                     vue.picUrl = response.data.sprites["front_shiny"];
 
                 })
